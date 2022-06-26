b43b33474affd1e7ee37fc6ca9b8b1da5da83146
Richard Joubert <richard@nsd.academy>
2022-06-26 10:24:15 -0400

    Update UninstallTest.ps1

--- Code Samples/Uninstall Apps/UninstallTest.ps1
+++ Code Samples/Uninstall Apps/UninstallTest.ps1
@@ -1,3 +1,5 @@
+# So far this has not worked
+
 Get-WmiObject -Class Win32_Product | Select-Object -Property Name
 
 $MyApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Free Tools"}
